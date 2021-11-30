da1f02e7e7eb05bc204c5fdb92180b7bf95a7929
boyicss123456 <61956136+boyicss123456@users.noreply.github.com>
2021-11-30 23:44:13 +0800

    fixed the stuck on each with new code now working

--- admin/forajax/print_session.php
+++ admin/forajax/print_session.php
@@ -1,39 +1,24 @@
 <?php
+
 session_start();
+
 $max=0;
-if(isset($_SESSION['cart']))
-{
-    $max = sizeof($_SESSION['cart']);
+
+if (isset($_SESSION['cart'])) {
+    //unset($_SESSION['cart']); // <- Yang Ni, Uncomment bila nak guna je.
+    $max = count($_SESSION['cart']);
 }
-for ($i = 0;$i < $max; $i++)
-{
+
+for ($i = 0; $i < $max; $i++) {
     if (isset($_SESSION['cart'][$i])) {
-        $company_name = "";
-        $product_name = "";
-        $unit = "";
-        $packing_size = "";
-        $qty="";
-        while (list ($key, $val) = each($_SESSION['cart'][$i]))
-        
-        {
-            if ($key == "company_name") {
-                $company_name = $val;
-            } else if ($key == "product_name") {
-                $product_name = $val;
-            } else if ($key == "unit") {
-                $unit = $val;
-            } else if ($key == "packing_size") {
-                $packing_size = $val;
-            }
-            else if($key=="qty")
-            {
-                $qty=$val;
-            }
-        }
-        echo $company_name." ".$product_name." ".$unit." ".$packing_size." ".$qty;
+        $company_name = $_SESSION['cart'][$i]['company_name'] ?? '';
+        $product_name = $_SESSION['cart'][$i]['product_name'] ?? '';
+        $unit = $_SESSION['cart'][$i]['unit'] ?? '';
+        $packing_size = $_SESSION['cart'][$i]['packing_size'] ?? '';
+        $qty = $_SESSION['cart'][$i]['qty'] ?? '';
+
+        echo $company_name . " " . $product_name . " " . $unit . " " . $packing_size . " " . $qty;
         echo "<br>";
     }
-
-
 }
 ?>
